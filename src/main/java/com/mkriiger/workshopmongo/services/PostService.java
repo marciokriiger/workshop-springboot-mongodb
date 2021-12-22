ec2875dcb09d292e556b1732b0f666449b94343c
Marcio Kriiger <marciovonkriiger@gmail.com>
2021-12-22 10:21:50 -0300

    Consulta com vários critérios

--- src/main/java/com/mkriiger/workshopmongo/services/PostService.java
+++ src/main/java/com/mkriiger/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.mkriiger.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,8 +23,14 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text) {
-		return postRepository.findByTitleCustom(text);
-		//return postRepository.findByTitleContainingIgnoreCase(text);
+		//return postRepository.findByTitleCustom(text);
+		return postRepository.findByTitleContainingIgnoreCase(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		//necessário acrescentar 24hs em milissegundos para que n busque apenas até o dia anterior
+		maxDate = new Date(maxDate.getTime() + (24 * 60 * 60 * 1000));
+		return postRepository.fullSearch(text, minDate, maxDate);
 	}
 	
 }
